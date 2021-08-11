6b1286bfdbc3b4243a99076f5f29851352adc04d
carlsromec <carlsromec@gmail.com>
2021-08-11 10:37:14 -0500

    add service config

---
+++ src/test/java/com/romec/configserver/StoreConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.romec.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
